9f6950be9934725d3a286d8de5735374a5f83ccc
Andrew Templeton <a.templeton@wearetuple.co>
2019-05-30 21:54:54 -0500

    Update to 1.0.0

--- auth.js
+++ auth.js
@@ -1,29 +1,21 @@
 
 const Crypto = require('crypto')
-const { OAuth } = require('oauth')
+const { OAuth2 } = require('oauth')
 const Qs = require('querystring')
-const Url = require('url')
 const Util = require('util')
-
 const { POST_AUTH_REDIRECT, SHOPIFY_API_KEY, SHOPIFY_API_SECRET } = process.env
-
 const scope = 'write_content, read_themes, write_themes, read_script_tags, write_script_tags, write_products'
 
-const getOAuthAccessToken = Util.promisify(function getOAuthAccessToken (shop, code, callback) {
-  return _OAuth(shop).getOAuthAccessToken(code, {}, (err, accessToken, refreshToken) => {
-    return callback(err, { accessToken, refreshToken })
-  })
+const getOAuthAccessToken = Util.promisify((shop, code, callback) => {
+  return Auth(shop).getOAuthAccessToken(code, {}, callback)
 })
 
-module.exports.init = hmacChecked(async ({ headers:{ Host }, queryStringParameters, requestContext:{ stage } }) => {
-  const { shop } = queryStringParameters
-  return redirect(_OAuth(shop).getAuthorizeUrl({ redirect_uri: `https://${Host}/${stage}/auth/token`, scope }))
+module.exports.init = hmacChecked(async ({ headers:{ Host }, queryStringParameters:{ shop } }) => {
+  return redirect(Auth(shop).getAuthorizeUrl({ redirect_uri: `https://${Host}/auth/token`, scope }))
 })
 
-module.exports.token = hmacChecked(async ({ queryStringParameters, headers: { Host }, requestContext: { stage } }) => {
-  const { code, shop } = queryStringParameters
-  const { accessToken, refreshToken } = await getOAuthAccessToken(shop, code)
-  return redirect((POST_AUTH_REDIRECT || `https://${Host}/${stage}/test`) + Qs.stringify({ accessToken, shop }))
+module.exports.token = hmacChecked(async ({ headers:{ Host }, queryStringParameters:{ shop, code } }) => {
+  return redirect((POST_AUTH_REDIRECT || `https://${Host}/test?`) + Qs.stringify({ accessToken: await getOAuthAccessToken(shop, code), shop }))
 })
 
 module.exports.page = async ({ queryStringParameters:{ shop, accessToken } }) => {
@@ -41,7 +33,7 @@ module.exports.page = async ({ queryStringParameters:{ shop, accessToken } }) =>
             shopOrigin: 'https://${shop}',
             debug: true
           });
-          ShopifyApp.flashNotice('You are using the shopify-serverless-auth default page. You can set what page to redirect to by setting the POST_AUTH_REDIRECT environment variable in the serverless.yml file.'');
+          ShopifyApp.flashNotice('You are using the shopify-serverless-auth default page. You can set what page to redirect to by setting the POST_AUTH_REDIRECT environment variable in the serverless.yml file.');
         </script>
       </head>
       <body>
@@ -52,12 +44,13 @@ module.exports.page = async ({ queryStringParameters:{ shop, accessToken } }) =>
   }
 }
 
-function redirect (uri) {
+function redirect (Location) {
+  console.log('Redirecting to: %s', Location)
   return {
     statusCode: 302,
     headers: {
       'Content-Type': 'text/html',
-      'Location': uri
+      Location
     }
   }
 }
@@ -65,12 +58,12 @@ function redirect (uri) {
 function hmacChecked (asyncFunctor) {
   return async ({ queryStringParameters, queryStringParameters:{ hmac }, headers, requestContext }) => {
     delete queryStringParameters.hmac
-    return Crypto.createHmac('sha256', SHOPIFY_API_SECRET).update(Qs.stringify(queryStringParameters)).digest('hex') !== hmac
-      ? { statusCode: 500 }
-      : await asyncFunctor({ queryStringParameters, headers, requestContext })
+    return Crypto.createHmac('sha256', SHOPIFY_API_SECRET).update(Qs.stringify(queryStringParameters)).digest('hex') == hmac
+      ? await asyncFunctor({ queryStringParameters, headers, requestContext })
+      : { statusCode: 500 }
   }
 }
 
-function _OAuth(origin) {
-  return new OAuth(SHOPIFY_API_KEY, SHOPIFY_API_SECRET, origin, '/admin/oauth/authorize', '/admin/oauth/access_token')
+function Auth(host) {
+  return new OAuth2(SHOPIFY_API_KEY, SHOPIFY_API_SECRET, `https://${host}`, '/admin/oauth/authorize', '/admin/oauth/access_token')
 }
